f8dd63477a776f235de4990e9afeea7449a61fec
return0927 <bjleh0927@gmail.com>
2018-05-05 01:09:41 +0900

    Changed requirements of `CustomExceptions`

--- el_engine/exceptions.py
+++ el_engine/exceptions.py
@@ -3,8 +3,9 @@ from time import time
 
 # Database Management
 class DBEngineNotFound(Exception):
-    def __init__(self, message, errors):
+    """def __init__(self, message=None, errors=None):
         super().__init__(message)
 
         self.errors = errors
-        self.raise_time = time()
+        self.raise_time = time()"""
+    pass
